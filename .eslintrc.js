d84b0d6e489c40268d4e0e59694ca87c16f59632
bvoelker <bvoelker@fluidpay.com>
2021-07-20 17:02:34 -0500

    eslint - updated linting usage

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,8 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard',
+  extends: [
+    'plugin:vue/vue3-recommended',
     '@vue/typescript'
   ],
   rules: {
@@ -13,13 +12,38 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 
     // Added to shut up linting for now
-    'camelcase': 'off',
+    camelcase: 'off',
     'no-useless-escape': 'off',
     'no-empty-pattern': 'off',
     'no-new': 'off',
 
-    // Vue ones
-    'vue/no-side-effects-in-computed-properties': 'off'
+    // Vue
+    'vue/order-in-components': ['error', {
+      order: [
+        'name',
+        ['components', 'directives', 'filters'],
+        ['props', 'propsData'],
+        'data',
+        'computed',
+        'watch',
+        'methods'
+      ]
+    }],
+
+    'vue/max-attributes-per-line': ['error', {
+      singleline: {
+        max: 5,
+        allowFirstLine: true
+      },
+      multiline: {
+        max: 1,
+        allowFirstLine: false
+      }
+    }],
+
+    'vue/component-tags-order': ['error', {
+      order: ['script', 'style', 'template']
+    }]
   },
   parserOptions: {
     parser: '@typescript-eslint/parser'
